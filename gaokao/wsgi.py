b2e55e4c53a1d47f9d563bfcd56ecc2dd0720877
feng_xia41103@hotmail.com <feng_xia41103@hotmail.com>
2015-02-06 03:48:48 +0800

    Apache2 deployment files modification.

--- gaokao/wsgi.py
+++ gaokao/wsgi.py
@@ -8,7 +8,22 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gaokao.settings")
+import sys
+import site
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/fengxia/.virtualenvs/gaokao/local/lib/python2.7/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/var/www/gkp')
+sys.path.append('/var/www/gkp/gaokao')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'gaokao.settings'
+
+# Activate your virtual env
+activate_env=os.path.expanduser("/home/fengxia/.virtualenvs/gaokao/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
