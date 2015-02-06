a1cfea4a37bb75a6ad7dbf18b3678032d4da1821
feng_xia41103@hotmail.com <feng_xia41103@hotmail.com>
2015-02-07 00:02:12 +0800

    Add libsass and using compressor to set up base.html to use SASS. Experimented with home page styles from HTML5.net but not impressive.

--- gaokao/settings.py
+++ gaokao/settings.py
@@ -110,6 +110,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, "static"),
 )
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    'compressor.finders.CompressorFinder',
+)
+
 MEDIA_ROOT = '/home/fengxia/Desktop/tt'
 STATIC_ROOT='/var/www/static'
 
