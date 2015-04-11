1bb31ed04e56cf36d69dc9771c08a6c73e6c9c11
feng_xia41103@hotmail.com <feng_xia41103@hotmail.com>
2015-04-11 14:18:01 -0400

    Adding tastypie and subclassed ModelResource so JSON can send utf-8 character correctly.

--- gaokao/urls.py
+++ gaokao/urls.py
@@ -2,8 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import settings
 
+# tastypie API urls
+from tastypie.api import Api
+from pi.api import *
+v1_api = Api(api_name='v1')
+v1_api.register(SchoolResource())
+
 urlpatterns = patterns('',
-    # Examples:
+    url(r'^api/', include(v1_api.urls)),
 	url(r'^gaokao/', include ('pi.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
